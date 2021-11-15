dc22ebf4b69e4684f17313b4fabe2209dc82b171
Thuan Gia Lam <duckladydinh@gmail.com>
2021-11-15 22:52:29 +0100

    Add gRPC server test.

--- src/test/java/com/giathuan/examples/fibonacci/FibonacciServiceTest.java
+++ src/test/java/com/giathuan/examples/fibonacci/FibonacciServiceTest.java
@@ -1,15 +1,69 @@
 package com.giathuan.examples.fibonacci;
 
+import io.grpc.Server;
+import io.grpc.Status.Code;
+import io.grpc.StatusRuntimeException;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /** ./gradlew test --tests com.giathuan.examples.fibonacci.FibonacciServiceTest. */
 public final class FibonacciServiceTest {
   @Test
-  public void dummy() {
-    assertEquals(2, 1 + 1);
-    Fibonacci fibonacci = Fibonacci.newBuilder().setIndex(0).setValue(1).build();
-		assertEquals(1, fibonacci.getValue());
+  public void getFibonacci_validIndex_returnsExpectedValue() throws IOException {
+    // arrange
+    String srvName = InProcessServerBuilder.generateName();
+
+    Server server =
+        InProcessServerBuilder.forName(srvName)
+            .directExecutor()
+            .addService(ProtoReflectionService.newInstance())
+            .addService(new FibonacciService())
+            .build();
+    server.start();
+
+    FibonacciServiceGrpc.FibonacciServiceBlockingStub stub =
+        FibonacciServiceGrpc.newBlockingStub(
+            InProcessChannelBuilder.forName(srvName).directExecutor().build());
+
+    // act
+    Fibonacci fibonacci = stub.getFibonacci(GetFibonacciRequest.newBuilder().setIndex(12).build());
+
+    // assert
+    assertEquals(144, fibonacci.getValue());
+  }
+
+  @Test
+  public void getFibonacci_invalidIndex_throwsInvalidArgument() throws IOException {
+    // arrange
+    String srvName = InProcessServerBuilder.generateName();
+
+    Server server =
+        InProcessServerBuilder.forName(srvName)
+            .directExecutor()
+            .addService(ProtoReflectionService.newInstance())
+            .addService(new FibonacciService())
+            .build();
+    server.start();
+
+    FibonacciServiceGrpc.FibonacciServiceBlockingStub stub =
+        FibonacciServiceGrpc.newBlockingStub(
+            InProcessChannelBuilder.forName(srvName).directExecutor().build());
+
+    // act
+    //noinspection ResultOfMethodCallIgnored
+    StatusRuntimeException e =
+        assertThrows(
+            StatusRuntimeException.class,
+            () -> stub.getFibonacci(GetFibonacciRequest.newBuilder().setIndex(-1).build()));
+
+    // assert
+    assertEquals(Code.INVALID_ARGUMENT, e.getStatus().getCode());
   }
 }
